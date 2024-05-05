f6183f8ec4a3465c8b7cda4d9e84f4a1e25c20fe
Weiss Bai <whitesgr03@gmail.com>
2024-05-05 19:51:03 +0800

    Create postController
    The "postList" middleware handles getting all posts and author name.
    
    The first middleware of "postDetail" calls "verifyParamId" to check
    parameter ID, and the second middleware handles getting a specific
    post and author name.
    
    The first middleware of "postCreate" calls "verifyToken" to check
    whether the user is logged in, the second middleware "verifyPermission"
    checks the user is an administrator or author, the third middleware
    "verifySchema" checks whether there are errors in each input field,
    and the last middleware handles adding new post.
    
    The "postUpdate" middleware is similar to the "postCreate" middleware.
    The difference is that "verifyParamId" is added in the second
    middleware position to check whether the parameter ID format is
    correct, and the last middleware handles updating a specific post.
    
    The "postDelete" middleware is similar to the "postUpdate" middleware.
    The difference is that "verifySchema" is not added in the fourth
    middleware position, and the last middleware handles deleting a
    specific post.

---
+++ controllers/postController.js
@@ -0,0 +1,184 @@
+const asyncHandler = require("express-async-handler");
+const { Types } = require("mongoose");
+
+const verifyToken = require("../utils/verifyToken");
+const verifyPermission = require("../utils/verifyPermission");
+const verifyParamId = require("../utils/verifyParamId");
+const verifySchema = require("../utils/verifySchema");
+
+const Post = require("../models/post");
+
+const postList = asyncHandler(async (req, res, next) => {
+	const posts = await Post.find({})
+		.populate("author", {
+			name: 1,
+			_id: 0,
+		})
+		.sort({ createdAt: 1 })
+		.exec();
+
+	res.json({
+		success: true,
+		message: "Get all posts successfully.",
+		data: posts,
+	});
+});
+const postDetail = [
+	verifyParamId,
+	asyncHandler(async (req, res, next) => {
+		const post = await Post.findById(req.params.id)
+			.populate("author", {
+				name: 1,
+				_id: 0,
+			})
+			.exec();
+
+		post
+			? res.json({
+					success: true,
+					message: "Get post successfully.",
+					data: post,
+			  })
+			: res.status(404).json({
+					success: false,
+					message: "The post cannot be found.",
+			  });
+	}),
+];
+const postCreate = [
+	verifyToken,
+	verifyPermission,
+	verifySchema({
+		title: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The title is required.",
+				bail: true,
+			},
+			isLength: {
+				options: { max: 100 },
+				errorMessage: "The title must be less than 100 long.",
+				bail: true,
+			},
+			escape: true,
+			custom: {
+				options: (title, { req }) =>
+					new Promise(async (resolve, reject) => {
+						const existingTitle = await Post.findOne({
+							title,
+						}).exec();
+						existingTitle
+							? reject((req.schema = { isConflict: true }))
+							: resolve();
+					}),
+				errorMessage: "The title is been used.",
+			},
+		},
+		content: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The content is required.",
+			},
+			escape: true,
+		},
+	}),
+	asyncHandler(async (req, res, next) => {
+		const currentTime = new Date();
+
+		const newPost = new Post({
+			...req.body,
+			author: req.user.id,
+			lastModified: currentTime,
+			createdAt: currentTime,
+		});
+
+		await newPost.save();
+
+		res.json({
+			success: true,
+			message: "Create post successfully.",
+		});
+	}),
+];
+const postUpdate = [
+	verifyToken,
+	verifyParamId,
+	verifyPermission,
+	verifySchema({
+		title: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The title is required.",
+				bail: true,
+			},
+			isLength: {
+				options: { max: 100 },
+				errorMessage: "The title must be less than 100 long.",
+				bail: true,
+			},
+			escape: true,
+			custom: {
+				options: (title, { req }) =>
+					new Promise(async (resolve, reject) => {
+						const existingTitle = await Post.findOne({
+							$and: [
+								{ title },
+								{
+									_id: {
+										$ne: Types.ObjectId.createFromHexString(
+											req.params.id
+										),
+									},
+								},
+							],
+						}).exec();
+						existingTitle
+							? reject((req.schema = { isConflict: true }))
+							: resolve();
+					}),
+				errorMessage: "The title is been used.",
+			},
+		},
+		content: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The content is required.",
+			},
+			escape: true,
+		},
+	}),
+	asyncHandler(async (req, res, next) => {
+		const newPost = {
+			...req.body,
+			lastModified: new Date(),
+		};
+
+		await Post.findByIdAndUpdate(req.params.id, newPost).exec();
+
+		res.json({
+			success: true,
+			message: "Update post successfully.",
+		});
+	}),
+];
+const postDelete = [
+	verifyToken,
+	verifyParamId,
+	verifyPermission,
+	asyncHandler(async (req, res, next) => {
+		await Post.findByIdAndDelete(req.params.id).exec();
+
+		res.json({
+			success: true,
+			message: "Delete post successfully.",
+		});
+	}),
+];
+
+module.exports = {
+	postList,
+	postDetail,
+	postCreate,
+	postUpdate,
+	postDelete,
+};
