67d7c5ffcbc65c71cff9b71b7ca5199fddd7df8f
Weiss Bai <whitesgr03@gmail.com>
2024-05-05 19:53:08 +0800

    Add post routes

--- routes/personal.js
+++ routes/personal.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const userControllers = require("../controllers/userController");
+const postControllers = require("../controllers/postController");
 
 router.post("/users", userControllers.userRegister);
 router.post("/users/login", userControllers.userLogin);
+
+router
+	.route("/posts")
+	.get(postControllers.postList)
+	.post(postControllers.postCreate);
+
+router
+	.route("/posts/:id")
+	.get(postControllers.postDetail)
+	.put(postControllers.postUpdate)
+	.delete(postControllers.postDelete);
 
 
 module.exports = router;
