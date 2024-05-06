268d954286978ed4eabe92104d9be095ab21b018
Weiss Bai <whitesgr03@gmail.com>
2024-05-06 14:05:17 +0800

    Create commentController
    The "commentList" middleware handles getting all comments for a
    specific id and author name.
    
    The first middleware of "commentCreate" calls "verifyToken" to check
    whether the user is logged in, the second middleware "verifyPermission"
    checks the user is an administrator or author, the third middleware
    "verifySchema" checks whether there are errors in each input field,
    and the last middleware handles adding new comment.
    
    The "commentUpdate" middleware is similar to the "commentCreate"
    middleware. The difference is that "verifyParamId" is added in the
    second middleware position to check whether the parameter ID format is
    correct, and the last middleware handles updating a specific comment.
    
    The "commentDelete" middleware is similar to the "commentUpdate"
    middleware. The difference is that "verifySchema" is not added in
    the fourth middleware position, and the last middleware handles
    deleting a specific comment.

---
+++ controllers/commentController.js
@@ -0,0 +1,122 @@
+const asyncHandler = require("express-async-handler");
+
+const verifyToken = require("../utils/verifyToken");
+const verifyPermission = require("../utils/verifyPermission");
+const verifyParamId = require("../utils/verifyParamId");
+const verifySchema = require("../utils/verifySchema");
+
+const Comment = require("../models/comment");
+
+const commentList = [
+	verifyParamId,
+	asyncHandler(async (req, res, next) => {
+		const comments = await Comment.find(
+			{ post: req.params.postId },
+			{ post: 0 }
+		)
+			.populate("author", {
+				name: 1,
+				_id: 0,
+			})
+			.sort({ createdAt: -1 })
+			.exec();
+
+		res.json({
+			success: true,
+			message: "Get all comments successfully.",
+			data: comments,
+		});
+	}),
+];
+const commentCreate = [
+	verifyToken,
+	verifyParamId,
+	verifySchema({
+		content: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The content is required.",
+				bail: true,
+			},
+			isLength: {
+				options: { max: 500 },
+				errorMessage: "The content must be less than 500 long.",
+			},
+			escape: true,
+		},
+	}),
+	asyncHandler(async (req, res, next) => {
+		const currentTime = new Date();
+
+		const newComment = new Comment({
+			...req.body,
+			author: req.user.id,
+			post: req.params.postId,
+			lastModified: currentTime,
+			createdAt: currentTime,
+		});
+
+		await newComment.save();
+
+		res.json({
+			success: true,
+			message: "Create comment successfully.",
+		});
+	}),
+];
+const commentUpdate = [
+	verifyToken,
+	verifyParamId,
+	verifyPermission,
+	verifySchema({
+		content: {
+			trim: true,
+			notEmpty: {
+				errorMessage: "The content is required.",
+				bail: true,
+			},
+			isLength: {
+				options: { max: 500 },
+				errorMessage: "The content must be less than 500 long.",
+			},
+			escape: true,
+		},
+	}),
+	asyncHandler(async (req, res, next) => {
+		const newComment = {
+			...req.body,
+			lastModified: new Date(),
+		};
+
+		await Comment.findByIdAndUpdate(
+			req.params.commentId,
+			newComment
+		).exec();
+
+		res.json({
+			success: true,
+			message: "Update comment successfully.",
+		});
+	}),
+];
+const commentDelete = [
+	verifyToken,
+	verifyParamId,
+	verifyPermission,
+	asyncHandler(async (req, res, next) => {
+		await Comment.findByIdAndDelete(req.params.commentId).exec();
+
+		res.json({
+			success: true,
+			message: "Delete comment successfully.",
+		});
+	}),
+];
+
+// 測試 comments
+module.exports = {
+	commentList,
+	commentCreate,
+	commentUpdate,
+	commentDelete,
+};
