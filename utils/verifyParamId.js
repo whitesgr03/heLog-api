79423aed9f2e97b2876acb6f1037cd03d9a06c29
Weiss Bai <whitesgr03@gmail.com>
2024-05-06 14:57:50 +0800

    Change verifyParamId middleware
    Add handler to verify comment id.

--- utils/verifyParamId.js
+++ utils/verifyParamId.js
@@ -2,11 +2,22 @@ const asyncHandler = require("express-async-handler");
 const { isValidObjectId } = require("mongoose");
 
 const verifyParamId = asyncHandler((req, res, next) => {
-	isValidObjectId(req.params.id)
+	const { postId, commentId } = req.params;
+
+	const isValidPostId = isValidObjectId(postId);
+	const isValidCommentId = isValidObjectId(commentId);
+
+	const isValidParamId = commentId
+		? isValidPostId && isValidCommentId
+		: isValidPostId;
+
+	isValidParamId
 		? next()
 		: res.status(404).json({
 				success: false,
-				message: "The post cannot be found.",
+				message: !isValidPostId
+					? "The post cannot be found."
+					: !isValidCommentId && "The comment cannot be found.",
 		  });
 });
 
