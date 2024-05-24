1cb09bc94020ebac43ea835301c0b952262ea195
Weiss Bai <whitesgr03@gmail.com>
2024-05-24 21:52:33 +0800

    Add publish schema property

--- models/post.js
+++ models/post.js
@@ -6,6 +6,7 @@ const PostSchema = new Schema({
 	author: { type: Schema.Types.ObjectId, ref: "User", required: true },
 	title: { type: String, required: true },
 	content: { type: String, required: true },
+	publish: { type: Boolean, required: true },
 	lastModified: { type: Date, required: true },
 	createdAt: { type: Date, immutable: true },
 });
