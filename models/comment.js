6e19b7eca90ec1c7860a2cc36c2c7148ef518dd7
Weiss Bai <whitesgr03@gmail.com>
2024-05-24 21:54:47 +0800

    Add immutable properties for author, post and reply

--- models/comment.js
+++ models/comment.js
@@ -3,12 +3,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-	author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+		immutable: true,
+	},
+	post: {
+		type: Schema.Types.ObjectId,
+		ref: "Post",
+		required: true,
+		immutable: true,
+	},
 	content: { type: String, required: true },
 	lastModified: { type: Date, required: true },
 	createdAt: { type: Date, immutable: true },
-	reply: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+	reply: { type: Schema.Types.ObjectId, ref: "Comment", immutable: true },
 });
 
 const CommentModel = mongoose.model("Comment", CommentSchema);
