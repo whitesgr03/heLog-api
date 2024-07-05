cd66314869b9be5a9ac445f75fe2ce317c36360d
Weiss Bai <whitesgr03@gmail.com>
2024-07-05 17:18:07 +0800

    Refactored verifyToken middleware

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,32 +1,66 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 
-const verifyToken = asyncHandler(async (req, res, next) => {
-	const token = req.headers.authorization;
+import { sessionStore } from "../config/database.js";
 
-	const handleErrorMessages = () => {
-		res.status(401).json({
-			success: false,
-			message:
-				"The access token provided is expired, revoked, malformed, or invalid for other reasons.",
+const verifyToken = [
+	asyncHandler((req, res, next) => {
+		const { authorization } = req.headers;
+		const token = authorization && authorization.split(" ")[1];
+		const handleSetLocals = () => {
+			const decode = jwt.decode(token);
+
+			req.payload = {
+				sid: decode?.sid,
+				scope: decode?.scope,
+			};
+			req.token = token;
+			next();
+		};
+
+		token
+			? handleSetLocals()
+			: res.status(400).json({
+					success: false,
+					message: "The access token provided is malformed.",
+			  });
+	}),
+	asyncHandler((req, res, next) => {
+		sessionStore.get(req.payload.sid, (err, session) => {
+			const handleSetLocals = () => {
+				req.user = { id: session.passport.user._id };
+				next();
+			};
+			err
+				? next(err)
+				: session
+				? handleSetLocals()
+				: res.status(401).json({
+						success: false,
+						message: "The request requires higher privileges.",
+				  });
 		});
-	};
+	}),
 
-	token
-		? jwt.verify(
-				token.split(" ")[1],
-				process.env.PRIVATE_KEY,
-				async (err, payload) => {
-					const setUserId = async () => {
-						req.user = {
-							id: payload.id,
-						};
-						next();
-					};
-					err ? handleErrorMessages() : setUserId();
-				}
-		  )
-		: handleErrorMessages();
-});
+	asyncHandler((req, res, next) => {
+		jwt.verify(
+			req.token,
+			process.env.JWT_SECRET,
+			{
+				subject: req.user.id,
+				issuer: process.env.ORIGIN,
+			},
+			err => {
+				err || !req.payload.scope
+					? res.status(401).json({
+							success: false,
+							message:
+								"The access token provided is expired, or invalid for other reasons.",
+					  })
+					: next();
+			}
+		);
+	}),
+];
 
 export default verifyToken;
