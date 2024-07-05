acd8bf1f9ae705c7d2aa8feac7b05aa67a42b254
Weiss Bai <whitesgr03@gmail.com>
2024-07-05 18:00:00 +0800

    Added tokenCreate middleware
    The middleware consists of 5 middlewares, first verify the body data,
    second get the auth code, third verify if the user is authenticated,
    fourth verify the code challenge and finally create the token and then
    redirect to the frontend.

--- controllers/authCodeController.js
+++ controllers/authCodeController.js
@@ -1,5 +1,10 @@
 import asyncHandler from "express-async-handler";
 import { randomBytes } from "node:crypto";
+import jwt from "jsonwebtoken";
+import { sessionStore } from "../config/database.js";
+
+import generateCodeChallenge from "../utils/generateCodeChallenge.js";
+import verifyToken from "../middlewares/verifyToken.js";
 import verifyQuery from "../middlewares/verifyQuery.js";
 
 import AuthCode from "../models/authCode.js";
@@ -77,3 +82,85 @@ const authToken = [
 		});
 	}),
 ];
+const tokenCreate = [
+	asyncHandler((req, res, next) => {
+		const { code, code_verifier } = req.body;
+
+		code && code_verifier
+			? next()
+			: res.status(400).json({
+					success: false,
+					message: "The request is missing a required parameter.",
+			  });
+	}),
+	asyncHandler(async (req, res, next) => {
+		const authCode = await AuthCode.findOne({
+			code: req.body.code,
+		}).exec();
+
+		const handleSetLocals = () => {
+			req.authCode = authCode;
+			next();
+		};
+
+		authCode
+			? handleSetLocals()
+			: res.status(400).json({
+					success: false,
+					message:
+						"The provided access grant is invalid, expired, or revoked.",
+			  });
+	}),
+	asyncHandler((req, res, next) => {
+		sessionStore.get(req.authCode.session, (err, session) => {
+			const handleSetLocals = async () => {
+				req.user = { id: session.passport.user._id };
+				next();
+			};
+			err
+				? next(err)
+				: session
+				? handleSetLocals()
+				: res.status(401).json({
+						success: false,
+						message: "The request requires higher privileges.",
+				  });
+		});
+	}),
+	asyncHandler((req, res, next) => {
+		const { code_verifier } = req.body;
+
+		generateCodeChallenge(code_verifier) === req.authCode.code_challenge
+			? next()
+			: res.status(400).json({
+					success: false,
+					message: "PKCE authentication failed.",
+			  });
+	}),
+	asyncHandler((req, res, next) => {
+		const oneMinute = 60 * 1000;
+
+		const token = jwt.sign(
+			{
+				sid: req.authCode.session,
+				scope: req.authCode.scope,
+			},
+			process.env.JWT_SECRET,
+			{
+				subject: req.user.id.toString(),
+				issuer: process.env.ORIGIN,
+				expiresIn: oneMinute,
+			}
+		);
+
+		res.json({
+			success: true,
+			message: "Get token successfully.",
+			data: {
+				access_token: token,
+			},
+		});
+	}),
+];
+
+export { authCode, authToken, tokenCreate };
