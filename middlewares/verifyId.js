b064a1f53c288ea114b767fb9ecb59232f460a32
Weiss Bai <whitesgr03@gmail.com>
2024-07-06 12:18:31 +0800

    Added two middlewares
    The first middleware ensures that the parameters are passed, and
    the last middleware ensures that the document with the specified
    id exists.

--- middlewares/verifyId.js
+++ middlewares/verifyId.js
@@ -1,7 +1,17 @@
 import asyncHandler from "express-async-handler";
 import { isValidObjectId } from "mongoose";
+import Post from "../models/post.js";
+import Comment from "../models/comment.js";
 
-const verifyId = name =>
+const verifyId = name => [
+	asyncHandler(async (req, res, next) => {
+		req.params[`${name}Id`]
+			? next()
+			: res.status(404).json({
+					success: false,
+					message: "The request is missing a required parameter.",
+			  });
+	}),
 	asyncHandler(async (req, res, next) => {
 		isValidObjectId(req.params[`${name}Id`])
 			? next()
@@ -9,6 +19,17 @@ const verifyId = name =>
 					success: false,
 					message: `The ${name} could not be found.`,
 			  });
-	});
+	}),
+	asyncHandler(async (req, res, next) => {
+		const Model = name === "post" ? Post : Comment;
+
+		(await Model.findById(req.params[`${name}Id`]).exec())
+			? next()
+			: res.status(404).json({
+					success: false,
+					message: `The ${name} could not be found.`,
+			  });
+	}),
+];
 
 export default verifyId;
