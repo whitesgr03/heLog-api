82c9d6faa4ce390bff7082b57fa5358f23be6363
Weiss Bai <whitesgr03@gmail.com>
2024-07-12 16:18:47 +0800

    Created verifyPermission middleware

---
+++ middlewares/verifyPermission.js
@@ -0,0 +1,17 @@
+import asyncHandler from "express-async-handler";
+
+import User from "../models/user.js";
+
+const verifyPermission = type =>
+	asyncHandler(async (req, res, next) => {
+		const user = await User.findById(req.user.id, { isAdmin: 1 }).exec();
+
+		user.isAdmin || user._id === req[type].authorId
+			? next()
+			: res.status(403).json({
+					success: false,
+					message: "This request requires higher permissions.",
+			  });
+	});
+
+export default verifyPermission;
