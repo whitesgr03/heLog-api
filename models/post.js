0779365e628ecbcafcc56afe54bce7697b610b1c
Weiss Bai <whitesgr03@gmail.com>
2024-07-25 19:13:28 +0800

    Updated post model
    Removed required properties for title and content.
    
    Added "mainImage" key for schema.
    
    Added virtual "mainImageUrl" to get "mainImage" source url.

--- models/post.js
+++ models/post.js
@@ -2,19 +2,35 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: "User",
-		required: true,
-		immutable: true,
+const PostSchema = new Schema(
+	{
+		author: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			immutable: true,
+		},
+		title: { type: String },
+		mainImage: { type: String },
+		content: { type: String },
+		publish: { type: Boolean, required: true },
+		lastModified: { type: Date, required: true },
+		createdAt: { type: Date, immutable: true },
 	},
-	title: { type: String, required: true },
-	content: { type: String, required: true },
-	publish: { type: Boolean, required: true },
-	lastModified: { type: Date, required: true },
-	createdAt: { type: Date, immutable: true },
-});
+	{
+		virtuals: {
+			mainImageUrl: {
+				get() {
+					const source = this.mainImage?.match(
+						/(?<=img src=")(.*?)(?=")/g
+					);
+					return source ? source[0] : null;
+				},
+			},
+		},
+		toJSON: { virtuals: true },
+	}
+);
 
 const PostModel = mongoose.model("Post", PostSchema);
 
