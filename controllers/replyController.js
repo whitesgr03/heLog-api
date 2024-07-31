4ce2f05a55ebe879dd31e732615cfdebccac2ac0
Weiss Bai <whitesgr03@gmail.com>
2024-07-31 12:52:29 +0800

    Updated replyList
    The first middleware JSON response message value has changed.

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -18,7 +18,7 @@ const replyList = [
 			? next()
 			: res.status(400).json({
 					success: false,
-					message: "The postId query is invalid object ID.",
+					message: "The postId query is invalid object id.",
 			  });
 	}),
 	asyncHandler(async (req, res, next) => {
