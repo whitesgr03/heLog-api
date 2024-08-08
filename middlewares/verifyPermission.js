3978ac89544b54983e470b76eb5b0e11622d1686
Weiss Bai <whitesgr03@gmail.com>
2024-08-08 12:44:38 +0800

    Add toSting method to condition

--- middlewares/verifyPermission.js
+++ middlewares/verifyPermission.js
@@ -6,7 +6,7 @@ const verifyPermission = type =>
 	asyncHandler(async (req, res, next) => {
 		const user = await User.findById(req.user.id, { isAdmin: 1 }).exec();
 
-		user.isAdmin || user._id === req[type].author._id
+		user.isAdmin || user._id.toString() === req[type].author._id.toString()
 			? next()
 			: res.status(403).json({
 					success: false,
