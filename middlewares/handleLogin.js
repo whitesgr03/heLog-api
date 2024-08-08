467021a53d8c8384669acab99b862d0c2492258c
Weiss Bai <whitesgr03@gmail.com>
2024-08-08 11:37:00 +0800

    Update handleLogin
    remove queries variable.
    
    create handleError and handleUserLogin.

--- middlewares/handleLogin.js
+++ middlewares/handleLogin.js
@@ -6,38 +6,31 @@ const handleLogin = asyncHandler((req, res, next) => {
 	const authenticateFn = passport.authenticate(
 		"local",
 		async (err, user, failInfo) => {
-			err && next(err);
-			const {
-				state,
-				code_challenge,
-				code_challenge_method,
-				redirect_url,
-				darkTheme,
-			} = req.query;
-			const queries =
-				`state=${state}` +
-				`&code_challenge=${code_challenge}` +
-				`&code_challenge_method=${code_challenge_method}` +
-				`&redirect_url=${redirect_url}` +
-				`&darkTheme=${darkTheme}`;
-
-			const csrf = new Csrf();
-			const secret = await csrf.secret();
-			req.session.csrf = secret;
-
-			failInfo &&
+			const handleError = async () => {
+				const csrf = new Csrf();
+				const secret = await csrf.secret();
+				req.session.csrf = secret;
 				res.render("login", {
 					user: req.data,
-					queries,
 					csrfToken: csrf.create(secret),
 					inputErrors: {
 						email: { msg: failInfo },
 					},
 				});
-			user &&
-				req.login(user, () => {
-					res.redirect(`/auth/code?${queries}`);
-				});
+			};
+
+			const handleUserLogin = () => {
+				const queries = req.session.queries;
+				const cb = () => {
+					req.session.queries = queries;
+					res.redirect(`/auth/code`);
+				};
+				req.login(user, cb);
+			};
+
+			err && next(err);
+			failInfo && (await handleError());
+			user && handleUserLogin();
 		}
 	);
 	authenticateFn(req, res, next);
