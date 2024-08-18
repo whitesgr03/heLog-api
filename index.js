96b9fe9861080ce60daa973ea2681501ff513b29
Weiss Bai <whitesgr03@gmail.com>
2024-08-18 15:21:43 +0800

    Update index.js
    Add "handleServer" function to start listening application.

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 import os from "node:os";
 import debug from "debug";
+import mongoose from "mongoose";
 
 import app from "./app.js";
-import db from "./config/database.js";
 
 const databaseLog = debug("Mongoose");
 const serverLog = debug("Server");
 
 const port = process.env.PORT || "3000";
 
-const handleListening = async () => {
-	const handlePrintNetwork = () => {
+const handleServer = () => {
+	databaseLog("MongoDB connecting successfully");
+	const handleListening = async () => {
 		const IP_Address = os
 			.networkInterfaces()
 			.en0.find(internet => internet.family === "IPv4").address;
-		serverLog(`Listening on Your Network:  http://${IP_Address}:${port}`);
+		serverLog(`Listening on Local:        http://localhost:${port}`);
+		serverLog(`Listening on Your Network: http://${IP_Address}:${port}`);
 	};
-
-	serverLog(`Listening on Local:         http://localhost:${port}`);
-	handlePrintNetwork();
+	const handleError = error => {
+		serverLog(`Server listening error.`);
+		switch (error.code) {
+			case "EACCES":
+				serverLog(`Port ${port} requires elevated privileges`);
+			case "EADDRINUSE":
+				serverLog(`Port ${port} is already in use`);
+			default:
+				serverLog(error);
+		}
+		app.close();
+	};
+	const handleClose = () => {
+		serverLog(`Server is closed.`);
+		mongoose.disconnect();
+		databaseLog(`Database is disconnected.`);
+		process.exit(1);
+	};
+	app.listen(port, process.env.NODE_ENV === "development" && handleListening)
+		.on("error", handleError)
+		.on("close", handleClose);
 };
 
-const handleError = error => {
-	switch (error.code) {
-		case "EACCES":
-			serverLog(`Port ${port} requires elevated privileges`);
-			process.exit(1);
-		case "EADDRINUSE":
-			serverLog(`Port ${port} is already in use`);
-			process.exit(1);
-		default:
-			throw error;
-	}
-};
 
-db.on("connected", () => {
-	databaseLog("Connecting successfully");
-	app.listen(
-		port,
-		process.env.NODE_ENV === "development" ? handleListening : {}
-	).on("error", handleError);
+
+mongoose.connection.on("connected", handleServer).on("error", err => {
+	databaseLog("Database error");
+	databaseLog(err);
+	app.close();
 });
