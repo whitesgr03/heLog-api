f4929be0fbc902eef62f19c5914a278fe6f8e270
Weiss Bai <whitesgr03@gmail.com>
2024-12-24 22:01:55 +0800

    feat!: remove middlewares
    Remove handleLogin, verifyAuthenticated, verifyFormSchema, verifyJSONSchema,
    verifyQuery and verifyToken middlewares.

--- middlewares/verifyAuthenticated.js
+++
@@ -1,7 +0,0 @@
-import asyncHandler from "express-async-handler";
-
-const verifyAuthenticated = asyncHandler((req, res, next) => {
-	!req.isAuthenticated() ? next() : res.redirect(process.env.HELOG_URL);
-});
-
-export default verifyAuthenticated;
