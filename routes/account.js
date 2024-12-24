638c08660ea211de74eaa562d3c09d1b52ba4f5f
Weiss Bai <whitesgr03@gmail.com>
2024-12-24 09:47:09 +0800

    feat!: delete basic password login route and registration route

--- routes/account.js
+++ routes/account.js
@@ -4,17 +4,6 @@ import * as accountControllers from "../controllers/accountController.js";
 
 export const accountRouter = express.Router();
 
-router
-	.route("/login")
-	.get(userControllers.userLoginGet)
-	.post(userControllers.userLoginPost);
-router
-	.route("/register")
-	.get(userControllers.userRegisterGet)
-	.post(userControllers.userRegisterPost);
-
-router.get("/logout", userControllers.userLogout);
-
 accountRouter.get("/login/google", accountControllers.googleLogin);
 accountRouter.get("/oauth2/redirect/google", accountControllers.googleRedirect);
 
