f5374ebd1f642f829afe03e43eb074f4ce7cdcee
Weiss Bai <whitesgr03@gmail.com>
2025-01-08 17:18:15 +0800

    feat!: schema will not check post id, comment id and reply id

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -42,72 +42,12 @@ export const replyCreate = [
 		content: {
 			trim: true,
 			notEmpty: {
-				errorMessage: "The content is required.",
+				errorMessage: "Content is required.",
 				bail: true,
 			},
 			isLength: {
 				options: { max: 500 },
-				errorMessage: "The content must be less than 500 long.",
-			},
-		},
-		post: {
-			trim: true,
-			notEmpty: {
-				errorMessage: "The post is required.",
-				bail: true,
-			},
-			custom: {
-				options: id => isValidObjectId(id),
-				errorMessage: "The post is invalid object ID.",
-				bail: true,
-			},
-			custom: {
-				options: id =>
-					new Promise(async (resolve, reject) => {
-						const isExisting = await Post.findById(id).exec();
-						isExisting ? resolve() : reject();
-					}),
-				errorMessage: "The post could not be found.",
-			},
-		},
-		comment: {
-			trim: true,
-			notEmpty: {
-				errorMessage: "The comment is required.",
-				bail: true,
-			},
-			custom: {
-				options: id => isValidObjectId(id),
-				errorMessage: "The comment is invalid object ID.",
-				bail: true,
-			},
-			custom: {
-				options: (id, { req }) =>
-					new Promise(async (resolve, reject) => {
-						const isExisting = await Comment.findById(id).exec();
-
-						isExisting && (!isExisting?.deleted || req.body.reply)
-							? resolve()
-							: reject();
-					}),
-				errorMessage: "The comment could not be found.",
-			},
-		},
-		reply: {
-			optional: true,
-			trim: true,
-			custom: {
-				options: id => isValidObjectId(id),
-				errorMessage: "The reply is invalid object ID.",
-				bail: true,
-			},
-			custom: {
-				options: id =>
-					new Promise(async (resolve, reject) => {
-						const isExisting = await Reply.findById(id).exec();
-						isExisting ? resolve() : reject();
-					}),
-				errorMessage: "The reply could not be found.",
+				errorMessage: "Content must be less than 500 long.",
 			},
 		},
 	}),
