d68fa5b62142569eeea67fadc1b6b7524718ced3
Weiss Bai <whitesgr03@gmail.com>
2025-02-20 22:44:52 +0800

    feat: added the clearAllCollections and createUsers to test setup file

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,5 +1,9 @@
 import { beforeEach, vi } from "vitest";
+import { clearAllCollections, createUsers } from "../lib/seed";
 
 beforeEach(async () => {
 	vi.clearAllMocks();
+
+	await clearAllCollections();
+	await createUsers({ amount: 2 });
 });
