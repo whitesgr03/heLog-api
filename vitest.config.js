d4fd4ce16bf90fb74ecd81bb741ee2a0ba5ac27c
Weiss Bai <whitesgr03@gmail.com>
2025-02-20 22:29:50 +0800

    chore: changed the include, exclude and setupFiles paths

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,9 @@ import { loadEnv } from "vite";
 
 export default defineConfig({
 	test: {
-		include: ["__test__/integration/**.test.js"],
-		exclude: ["__test__/integration/delete_**.test.js"],
-		setupFiles: ["__test__/integration/setup.js"],
+		include: ["__test__/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+		exclude: ["__test__/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)"],
+		setupFiles: "./__test__/setup.js",
 		env: loadEnv("", process.cwd(), ""),
 	},
 });
