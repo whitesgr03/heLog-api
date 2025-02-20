bb3a307ab206a8612bc14cc4499d69008e92f5d3
Weiss Bai <whitesgr03@gmail.com>
2025-02-20 22:27:30 +0800

    chore: added the env option

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 
 export default defineConfig({
 	test: {
 		include: ["__test__/integration/**.test.js"],
 		exclude: ["__test__/integration/delete_**.test.js"],
 		setupFiles: ["__test__/integration/setup.js"],
+		env: loadEnv("", process.cwd(), ""),
 	},
 });
