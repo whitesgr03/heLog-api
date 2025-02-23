b3c51034d0cd8ddb65cc20c64aec744d05532ab0
Weiss Bai <whitesgr03@gmail.com>
2025-02-24 00:56:17 +0800

    feat: added the beforeAll and afterAll method
    Displays messages when all tests are started and completed.

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,9 +1,21 @@
-import { beforeEach, vi } from "vitest";
+import { afterAll, beforeEach, beforeAll, vi } from "vitest";
+import { mongoose } from "../config/database.js";
 import { clearAllCollections, createUsers } from "../lib/seed";
+import debug from "debug";
+
+const databaseLog = debug("Mongoose");
+
+beforeAll(async () => {
+	databaseLog("MongoDB is connected.");
+});
 
 beforeEach(async () => {
 	vi.clearAllMocks();
-
 	await clearAllCollections();
 	await createUsers({ amount: 2 });
+});
+
+afterAll(() => {
+	mongoose.disconnect();
+	databaseLog("MongoDB is disconnected.");
 });
