62d8b0d321e7cc11cd79be43f739b76c0e1d998f
Weiss Bai <whitesgr03@gmail.com>
2025-05-25 18:17:07 +0800

    feat: created and exported a modal type

--- src/models/post.ts
+++ src/models/post.ts
@@ -2,23 +2,25 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const postSchema = {
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+		immutable: true,
+	},
+	title: { type: String },
+	mainImage: { type: String },
+	content: { type: String },
+	publish: { type: Boolean, default: false },
+};
+
+export type PostDocument = mongoose.Document &
+	mongoose.InferRawDocType<typeof postSchema>;
+
 export const Post = mongoose.model(
 	"Post",
-	new Schema(
-		{
-			author: {
-				type: Schema.Types.ObjectId,
-				ref: "User",
-				required: true,
-				immutable: true,
-			},
-			title: { type: String },
-			mainImage: { type: String },
-			content: { type: String },
-			publish: { type: Boolean, default: false },
-		},
-		{
-			timestamps: true,
-		}
-	)
+	new Schema<PostDocument>(postSchema, {
+		timestamps: true,
+	})
 );
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,20 +2,22 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const userSchema = {
+	email: {
+		type: String,
+		required: true,
+		immutable: true,
+		lowercase: true,
+	},
+	username: { type: String, required: true },
+	isAdmin: { type: Boolean, required: true, immutable: true },
+	provider: [{ type: String, required: true }],
+};
+
+export type UserDocument = mongoose.Document &
+	mongoose.InferRawDocType<typeof userSchema>;
+
 export const User = mongoose.model(
 	"User",
-	new Schema(
-		{
-			email: {
-				type: String,
-				required: true,
-				immutable: true,
-				lowercase: true,
-			},
-			username: { type: String, required: true },
-			isAdmin: { type: Boolean, immutable: true },
-			provider: { type: Array, required: true },
-		},
-		{ timestamps: true }
-	)
+	new Schema<UserDocument>(userSchema, { timestamps: true })
 );
