782964a40fbd7f7f565bdf4316bde49b7e9b50f5
Weiss Bai <whitesgr03@gmail.com>
2025-06-02 12:59:21 +0800

    feat: added connected, disconnected and error event

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,14 +3,19 @@ import mongoose from "mongoose";
 
 const databaseLog = debug("Mongoose");
 
-mongoose.connection.on("connecting", () =>
-	databaseLog("MongoDB connecting ...")
-);
+mongoose.connection
+	.on("connecting", () => databaseLog("MongoDB connecting ..."))
+	.on("connected", () => databaseLog("MongoDB is connected."))
+	.on("disconnected", () => databaseLog("MongoDB is disconnected."))
+	.on("error", err => {
+		databaseLog("Database has some error occurs");
+		databaseLog(err);
+	});
 
 mongoose.connect(process.env.DATABASE_STRING ?? "").catch((err: Error) => {
 	databaseLog("Database connecting error");
 	databaseLog(err);
-	mongoose.connection.close();
+	mongoose.disconnect();
 });
 
 export { mongoose };
