579a208a32a7499fa87343a4132929d29e8f7384
Weiss Bai <whitesgr03@gmail.com>
2025-06-02 13:00:44 +0800

    test: changed the login method

--- src/__test__/integration/user.test.js
+++ src/__test__/integration/user.test.js
@@ -28,17 +28,21 @@ app.use(
 app.use(passport.session());
 app.use(express.json());
 
-app.get("/login", (req, res, next) => {
+app.post("/login", (req, res, next) => {
 	req.body = {
-		admin: req.query.isAdmin ?? "1",
-		_: " ",
+		...req.body,
+		password: " ",
 	};
 	passport.authenticate("local", (_err, user) => {
-		req.login(user, () => {
-			res.send({
-				token: generateCSRFToken(req.sessionID),
-			});
-		});
+		user
+			? req.login(user, () => {
+					res.send({
+						token: generateCSRFToken(req.sessionID),
+					});
+			  })
+			: res.status(404).send({
+					message: "The user is not found.",
+			  });
 	})(req, res, next);
 });
 
@@ -57,44 +61,100 @@ describe("User paths", () => {
 		});
 	});
 	describe("GET /posts", () => {
-		it(`should response with all posts of the authenticate user`, async () => {
-			const authenticatedUser = await User.findOne().exec();
+		it(`should response with all posts of a specified user`, async () => {
+			const user = await User.findOne().exec();
 
 			const mockPosts = await createPosts({
-				users: [authenticatedUser],
+				users: [user],
 				amount: 3,
 			});
 
 			const agent = request.agent(app);
 
-			await agent.get(`/login`);
+			await agent.post(`/login`).send({ username: user.username });
 
 			const { status, body } = await agent.get(`/posts`);
 
 			expect(status).toBe(200);
 			expect(body.success).toBe(true);
 			expect(body.message).toBe("Get user's post list successfully.");
-			expect(body.data.length).toBe(mockPosts.length);
+
+			expect(body.data.userPostsCount).toBe(mockPosts.length);
+
+			const postsTitles = mockPosts.map(post => post.title);
+			body.data.userPosts.forEach(post => {
+				expect(postsTitles).toContain(post.title);
+			});
+		});
+	});
+	describe("GET /posts/:postId", () => {
+		it(`should response with with a 404 status, if a specified post of the user is not found`, async () => {
+			const user = await User.findOne().exec();
+
+			await createPosts({
+				users: [user],
+				amount: 2,
+			});
+
+			const mockPost = {
+				_id: "test123",
+			};
+
+			const agent = request.agent(app);
+
+			await agent.post(`/login`).send({ username: user.username });
+
+			const { status, body } = await agent.get(`/posts/${mockPost._id}`);
+
+			expect(status).toBe(404);
+			expect(body.success).toBe(false);
+			expect(body.message).toBe(`Post could not be found.`);
+		});
+		it(`should response with a specified post of the authenticate user`, async () => {
+			const user = await User.findOne({}).exec();
+
+			const mockPosts = await createPosts({
+				users: [user],
+				amount: 2,
+			});
+
+			const mockPost = mockPosts[0];
+
+			const agent = request.agent(app);
+
+			await agent.post(`/login`).send({ username: user.username });
+
+			const { status, body } = await agent.get(`/posts/${mockPost._id}`);
+
+			expect(status).toBe(200);
+			expect(body.success).toBe(true);
+			expect(body.message).toBe("Get post successfully.");
+
+			expect(body.data._id).toBe(mockPost.id);
 		});
 	});
 	describe("Verify CSRF token", () => {
-		it("should respond with a 403 status code and message if a CSRF custom header is invalid", async () => {
+		it("should respond with a 403 status code and message if a CSRF token is not provided", async () => {
+			const user = await User.findOne({}).exec();
+
 			const agent = request.agent(app);
 
-			await agent.get(`/login`);
+			await agent.post(`/login`).send({ username: user.username });
 
 			const { status, body } = await agent.get(`/`);
 
 			expect(status).toBe(403);
 			expect(body).toStrictEqual({
 				success: false,
-				message: "CSRF custom header is invalid.",
+				message: "CSRF token mismatch.",
 			});
 		});
-		it("should respond with a 403 status code and message if a CSRF custom header send by client mismatch", async () => {
+		it("should respond with a 403 status code and message if a CSRF token send by client but mismatch", async () => {
+			const user = await User.findOne({}).exec();
+
 			const agent = request.agent(app);
 
-			await agent.get(`/login`);
+			await agent.post(`/login`).send({ username: user.username });
 
 			const { status, body } = await agent
 				.get(`/`)
@@ -108,12 +168,14 @@ describe("User paths", () => {
 		});
 	});
 	describe("GET /", () => {
-		it(`should response with the authenticate user detail`, async () => {
-			const authenticatedUser = await User.findOne().exec();
+		it(`should response with a specified user detail`, async () => {
+			const user = await User.findOne().exec();
 
 			const agent = request.agent(app);
 
-			const loginResponse = await agent.get(`/login`);
+			const loginResponse = await agent
+				.post(`/login`)
+				.send({ username: user.username });
 
 			const [token, value] = loginResponse.body.token.split(".");
 
@@ -124,16 +186,20 @@ describe("User paths", () => {
 			expect(status).toBe(200);
 			expect(body.success).toBe(true);
 			expect(body.message).toBe("Get user info successfully.");
-			expect(body.data.username).toBe(authenticatedUser.username);
-			expect(body.data.isAdmin).toBe(authenticatedUser.isAdmin);
-			expect(body.data.email).toBe(authenticatedUser.email);
+			expect(body.data.username).toBe(user.username);
+			expect(body.data.isAdmin).toBe(user.isAdmin);
+			expect(body.data.email).toBe(user.email);
 		});
 	});
 	describe("PATCH /", () => {
 		it(`should respond with a 400 status code and error fields message if a new username is not provided`, async () => {
+			const user = await User.findOne().exec();
+
 			const agent = request.agent(app);
 
-			const loginResponse = await agent.get(`/login`);
+			const loginResponse = await agent
+				.post(`/login`)
+				.send({ username: user.username });
 
 			const [token, value] = loginResponse.body.token.split(".");
 
@@ -146,11 +212,13 @@ describe("User paths", () => {
 			expect(body.fields).toHaveProperty("username");
 		});
 		it(`should respond with a 409 status code and message if a new username exists`, async () => {
-			const [, secondUser] = await User.find({}).exec();
+			const [user, secondUser] = await User.find({}).exec();
 
 			const agent = request.agent(app);
 
-			const loginResponse = await agent.get(`/login`);
+			const loginResponse = await agent
+				.post(`/login`)
+				.send({ username: user.username });
 
 			const [token, value] = loginResponse.body.token.split(".");
 
@@ -162,14 +230,18 @@ describe("User paths", () => {
 
 			expect(status).toBe(409);
 			expect(body.success).toBe(false);
-			expect(body.message).toBe("Username is been used.");
+			expect(body.fields.username).toBe("Username is been used.");
 		});
-		it(`should update the authenticate user's username to a new username`, async () => {
+		it(`should update the user's username to a new username`, async () => {
+			const user = await User.findOne().exec();
+
 			const mockNewUsername = faker.person.middleName();
 
 			const agent = request.agent(app);
 
-			const loginResponse = await agent.get(`/login`);
+			const loginResponse = await agent
+				.post(`/login`)
+				.send({ username: user.username });
 
 			const [token, value] = loginResponse.body.token.split(".");
 
@@ -186,7 +258,7 @@ describe("User paths", () => {
 		});
 	});
 	describe("DELETE /", () => {
-		it(`should delete the authenticate user and all posts and comments of that user, then log out.`, async () => {
+		it(`should delete the user and all posts and comments of that user, then log out.`, async () => {
 			const user = await User.findOne().exec();
 
 			vi.spyOn(Post, "deleteOne");
@@ -200,7 +272,9 @@ describe("User paths", () => {
 			});
 			const agent = request.agent(app);
 
-			const loginResponse = await agent.get(`/login`);
+			const loginResponse = await agent
+				.post(`/login`)
+				.send({ username: user.username });
 
 			const [token, value] = loginResponse.body.token.split(".");
 
