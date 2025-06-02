6b10a561fb89de62d0acfe4b968e6876b3f62104
Weiss Bai <whitesgr03@gmail.com>
2025-06-02 08:36:50 +0800

    chore: changed the test options

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { loadEnv } from "vite";
 
 export default defineConfig({
 	test: {
-		include: ["__test__/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
-		exclude: ["__test__/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)"],
-		setupFiles: "./__test__/setup.js",
+		include: ["src/__test__/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+		exclude: ["src/__test__/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)"],
+		setupFiles: "src/__test__/setup.js",
 		env: loadEnv("", process.cwd(), ""),
 		fileParallelism: false,
 	},
