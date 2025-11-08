15daf785db6f83520df6b104370ce20d8d4dac30
Weiss Bai <whitesgr03@gmail.com>
2025-11-08 18:44:15 +0800

    feat: add federated model to store social login information

---
+++ src/models/federated.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const federatedSchema = {
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+		immutable: true,
+	},
+	provider: {
+		type: String,
+		required: true,
+		immutable: true,
+	},
+	subject: { type: String, required: true, immutable: true },
+};
+
+export type FederatedDocument = mongoose.Document &
+	mongoose.InferRawDocType<typeof federatedSchema>;
+
+
+
+export const Federated = mongoose.model(
+	"Federated",
+	new Schema<FederatedDocument>(federatedSchema, { timestamps: true })
+);
