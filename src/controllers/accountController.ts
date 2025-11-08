cf30d27c156a76932b4b653f9a9aafdd064d8d0b
Weiss Bai <whitesgr03@gmail.com>
2025-11-08 19:34:43 +0800

    feat!: update the googleRedirect controller
    If the user logs in successfully, then redirect to helog website.

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -10,37 +10,27 @@ export const googleLogin: RequestHandler = passport.authenticate("google");
 export const googleRedirect: RequestHandler[] = [
 	(req, res, next) => {
 		const authenticateCb: AuthenticateCallback = (err, user) => {
-			const redirect_origin =
-				process.env
-					.ALLOW_CLIENT_ORIGINS!.split(",")
-					.find(origin => `${origin}/` === req.session.referer) ??
-				process.env.HELOG_URL!;
-
-			delete req.session.referer;
-
 			err && next(err);
-			user
-				? req.login(user, () => {
-						res.set("Cache-Control", "no-cache=Set-Cookie") // To avoid the private or sensitive data exchanged within the session through the web browser cache after the session has been closed.
-							.cookie(
-								process.env.NODE_ENV === "production"
-									? "__Secure-token"
-									: "token",
-								generateCSRFToken(req.sessionID),
-								{
-									sameSite: "strict",
-									httpOnly: false,
-									secure:
-										process.env.NODE_ENV === "production",
-									domain: process.env.DOMAIN ?? "",
-									maxAge:
-										req.session.cookie.originalMaxAge ??
-										Date.now(),
-								}
-							)
-							.redirect(redirect_origin);
-				  })
-				: res.redirect(redirect_origin);
+			user &&
+				req.login(user, () => {
+					res.set("Cache-Control", "no-cache=Set-Cookie") // To avoid the private or sensitive data exchanged within the session through the web browser cache after the session has been closed.
+						.cookie(
+							process.env.NODE_ENV === "production"
+								? "__Secure-token"
+								: "token",
+							generateCSRFToken(req.sessionID),
+							{
+								sameSite: "strict",
+								httpOnly: process.env.NODE_ENV === "production",
+								secure: process.env.NODE_ENV === "production",
+								domain: process.env.DOMAIN ?? "",
+								maxAge:
+									req.session.cookie.originalMaxAge ??
+									Date.now(),
+							}
+						)
+						.redirect(process.env.HELOG_URL!);
+				});
 		};
 
 		const authenticateFn = passport.authenticate("google", authenticateCb);
