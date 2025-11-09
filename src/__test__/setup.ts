73022eb63d2fd7cd2164d4a271f371fcc1b7886b
Weiss Bai <whitesgr03@gmail.com>
2025-11-10 01:37:59 +0800

    chore: rename createUsers to createFakeUsers

--- src/__test__/setup.ts
+++ src/__test__/setup.ts
@@ -1,11 +1,11 @@
 import { afterAll, beforeEach, vi } from "vitest";
 import { mongoose } from "../config/database.js";
-import { clearAllCollections, createUsers } from "../lib/seed.js";
+import { clearAllCollections, createFakeUsers } from "../lib/seed.js";
 
 beforeEach(async () => {
 	vi.clearAllMocks();
 	await clearAllCollections({ forTesting: true });
-	await createUsers({ amount: 2, forTesting: true });
+	await createFakeUsers({ amount: 2, forTesting: true });
 });
 
 afterAll(() => {
