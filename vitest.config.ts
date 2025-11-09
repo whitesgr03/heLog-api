7c447ed639c0d801a81073b4f89366f773cc08c8
Weiss Bai <whitesgr03@gmail.com>
2025-11-10 01:36:57 +0800

    chore: update env property

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		include: ["src/__test__/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
 		exclude: ["src/__test__/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)"],
 		setupFiles: "src/__test__/setup.js",
-		env: loadEnv("", process.cwd(), ""),
+		env: loadEnv("test", process.cwd(), ""),
 		fileParallelism: false,
 	},
 });
