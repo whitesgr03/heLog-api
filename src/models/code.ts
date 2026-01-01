fe1100c04a0b23a10a633d96a860b3c25184cdb1
Weiss Bai <whitesgr03@gmail.com>
2026-01-01 20:50:52 +0800

    feat!: remove newUser, verify and failCount properties

--- src/models/code.ts
+++ src/models/code.ts
@@ -3,15 +3,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const codeSchema = {
-	newUser: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		immutable: true,
-	},
 	code: { type: String, required: true },
 	email: { type: String, required: true, immutable: true },
-	verify: { type: Boolean, default: false },
-	failCount: { type: Number, default: 0 },
 	expiresAfter: { type: Date },
 };
 
