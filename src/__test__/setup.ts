55625f72f9e0c63dc5db34c4c7eaf6091df6d0cf
Weiss Bai <whitesgr03@gmail.com>
2026-01-14 20:32:48 +0800

    feat!: use the resetAllMocks method instead of clearAllMocks

--- src/__test__/setup.ts
+++ src/__test__/setup.ts
@@ -3,7 +3,7 @@ import { mongoose } from '../config/database.js';
 import { clearAllCollections, createFakeUsers } from '../lib/seed.js';
 
 beforeEach(async () => {
-	vi.clearAllMocks();
+	vi.resetAllMocks();
 	await clearAllCollections({ forTesting: true });
 	await createFakeUsers({ amount: 2, forTesting: true });
 });
