f9931aae7827849518467833708fa9879165afc2
Weiss Bai <whitesgr03@gmail.com>
2026-05-06 18:57:57 +0800

    refactor: remove condition with throw error message

--- src/utils/generateCSRFToken.ts
+++ src/utils/generateCSRFToken.ts
@@ -1,11 +1,7 @@
 import { randomBytes, createHmac } from 'node:crypto';
 
 export const generateCSRFToken = (sessionId: string) => {
-	const secret =
-		process.env.CSRF_SECRETS ??
-		(() => {
-			throw Error('An environment of CSRF_SECRETS is required');
-		})();
+	const secret = process.env.CSRF_SECRETS!;
 	const randomValue = randomBytes(64).toString('hex');
 	const message = `${sessionId.length}!${sessionId}!${randomValue.length}!${randomValue}`;
 	const hmac = createHmac('sha256', secret).update(message).digest('hex');
