f14b790568aea0bd01915af77d1bae9a8b85fb16
Weiss Bai <whitesgr03@gmail.com>
2026-05-07 16:08:30 +0800

    refactor: improve error handling and debug message

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import mongoose from 'mongoose';
-import { serverLog } from '../src/config/debug.js';
+import { server } from './utils/loggers.js';
 import { app } from './app.js';
 
 const port = process.env.PORT || 4000;
 
 mongoose.connection.on('connected', () => {
 	app
-		.listen(port, () => serverLog(`Server is listened`))
-		.on('error', (error: { code?: string }) => {
-			switch (error.code) {
-				case 'EACCES':
-					serverLog(`Port ${port} requires elevated privileges`);
-					break;
-				case 'EADDRINUSE':
-					serverLog(`Port ${port} is already in use`);
-					break;
-				default:
-					serverLog('process.exit', error);
-					process.exit(1);
+		.listen(port, () => server(`is listened.`))
+		.on('error', error => {
+			server(`has an error occur.`);
+			if ('code' in error) {
+				switch (error.code) {
+					case 'EACCES':
+						server(`port ${port} requires elevated privileges`);
+						break;
+					case 'EADDRINUSE':
+						server(`port ${port} is already in use`);
+						break;
+					default:
+						server(`process exit with error code: ${error.code}`);
+						process.exit(1);
+				}
+			} else {
+				if (error instanceof Error) server(`error message: ${error.message}`);
+				server(`error detail: ${error}`);
 			}
 		});
 });
