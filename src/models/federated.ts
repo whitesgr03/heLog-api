e1eb992be8589a055ca4621c075376d1b9c3ae37
Weiss Bai <whitesgr03@gmail.com>
2026-05-07 12:49:28 +0800

    refactor: remove model types

--- src/models/federated.ts
+++ src/models/federated.ts
@@ -1,26 +1,21 @@
-import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const Schema = mongoose.Schema;
-
-const federatedSchema = {
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-		immutable: true,
+const federatedSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+			immutable: true,
+		},
+		provider: {
+			type: String,
+			required: true,
+			immutable: true,
+		},
+		subject: { type: String, required: true, immutable: true },
 	},
-	provider: {
-		type: String,
-		required: true,
-		immutable: true,
-	},
-	subject: { type: String, required: true, immutable: true },
-};
-
-export type FederatedDocument = mongoose.Document &
-	mongoose.InferRawDocType<typeof federatedSchema>;
-
-export const Federated = mongoose.model(
-	'Federated',
-	new Schema<FederatedDocument>(federatedSchema, { timestamps: true }),
+	{ timestamps: true },
 );
+
+export const Federated = model('Federated', federatedSchema);
