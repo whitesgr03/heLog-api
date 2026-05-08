10ed4c085b11d415cba973974ba8041e41a9b91e
Weiss Bai <whitesgr03@gmail.com>
2026-05-08 13:57:32 +0800

    refactor: export a reuseable function instead of side-effects import
    The checkEnv function receives an array of string.
    
    Remove ENV_VARS variables.

--- src/utils/checkEnv.ts
+++ src/utils/checkEnv.ts
@@ -1,30 +1,17 @@
 import { server } from './loggers.js';
 
-const ENV_VARS = [
-	'DB',
-	'SERVER',
-	'FACEBOOK_CLIENT_ID',
-	'FACEBOOK_CLIENT_SECRET',
-	'GOOGLE_CLIENT_ID',
-	'GOOGLE_CLIENT_SECRET',
-	'HELOG_API_URL',
-	'HELOG_URL',
-	'DATABASE_STRING',
-	'DOMAIN',
-	'CSRF_SECRETS',
-	'MAILGUN_API_KEY',
-	'MAILGUN_DOMAIN',
-] as const;
-
-try {
-	ENV_VARS.forEach(variable => {
-		if (!process.env[variable]) {
-			throw Error(`environment variable ${variable} is missing.`);
+export default function checkEnv(vars: readonly string[]) {
+	server('checking environment variables...');
+	try {
+		vars.forEach(variable => {
+			if (!process.env[variable]) {
+				throw Error(`environment variable ${variable} is missing.`);
+			}
+		});
+	} catch (error) {
+		if (error instanceof Error) {
+			server(`process exit because ${error.message}`);
+			process.exit(1);
 		}
-	});
-} catch (error) {
-	if (error instanceof Error) {
-		server(`process exit because ${error.message}`);
-		process.exit(1);
 	}
 }
