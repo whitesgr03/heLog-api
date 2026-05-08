0b915719937217bb4817d2525275d6e355dbd9f9
Weiss Bai <whitesgr03@gmail.com>
2026-05-08 15:16:18 +0800

    refactor: use index.ts handles initialization

--- src/app.ts
+++ src/app.ts
@@ -11,136 +11,137 @@ import { limiterBruteForceByIp } from './utils/rateLimiter.js';
 import { server } from './utils/loggers.js';
 import path from 'node:path';
 
-// configs
-import './config/passport.js';
-import './config/database.js';
-
 // routes
 import { accountRouter } from './routes/account.js';
 import { blogRouter } from './routes/blog.js';
 import { userRouter } from './routes/user.js';
 
-export const app = express();
+const createApp = () => {
+	const app = express();
 
-app.get('/favicon.ico', (_req, res) => {
-	res.sendStatus(204);
-});
+	app.get('/favicon.ico', (_req, res) => {
+		res.sendStatus(204);
+	});
 
-app.use(async (req, res, next) => {
-	try {
-		if (process.env.NODE_ENV !== 'development') {
-			await limiterBruteForceByIp.consume(req.ip as string);
+	app.use(async (req, res, next) => {
+		try {
+			if (process.env.NODE_ENV !== 'development') {
+				await limiterBruteForceByIp.consume(req.ip as string);
+			}
+			next();
+		} catch (rejected) {
+			if (rejected instanceof RateLimiterRes) {
+				res.status(429).json({
+					success: false,
+					message: 'Too many requests',
+				});
+			} else {
+				next(rejected);
+			}
+		}
+	});
+
+	const corsOptions = {
+		origin:
+			process.env.NODE_ENV === 'production'
+				? /helog\.whitesgr03\.com$/
+				: /localhost/,
+		methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+		credentials: true,
+		allowedHeaders: ['Content-Type', 'X-CSRF-TOKEN'],
+		exposedHeaders: ['Retry-After', 'Expire-After'],
+		maxAge: 10,
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+	};
+	const sessionOptions: SessionOptions = {
+		secret: process.env.SESSION_SECRETS?.split(','),
+		resave: false,
+		saveUninitialized: false, // If the user first send request to the server, at the end of the request and when saveUninitialized is false, the req.session is unmodified then will not be stored in the session store.
+		store: sessionStore.create({
+			client: mongoose.connection.getClient(),
+			stringify: false,
+		}),
+		name: process.env.NODE_ENV === 'production' ? '__Secure-id' : 'id',
+		cookie: {
+			sameSite: 'strict',
+			domain: process.env.DOMAIN ?? '',
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+		},
+	};
+	const helmetOptions: HelmetOptions = {
+		xFrameOptions: { action: 'deny' }, // To avoid clickjacking attacks, by ensuring that their content is not embedded into other sites.
+		strictTransportSecurity: {
+			//  It should only be accessed using HTTPS, instead of using HTTP.
+			maxAge: 63072000,
+			preload: true,
+		},
+		crossOriginEmbedderPolicy: true, // A document can only load resources from the same origin.
+		crossOriginResourcePolicy: { policy: 'same-site' }, // Limit current resource loading to the site and sub-domains only.
+		xPoweredBy: false,
+	};
+	const staticOptions = {
+		maxAge: '1d',
+		index: false,
+		redirect: false,
+	};
+
+	app.set('trust proxy', 1);
+	app.disable('x-powered-by');
+
+	app.use(cors(corsOptions));
+	app.use(helmet(helmetOptions));
+	app.use(session(sessionOptions));
+	app.use(passport.session());
+	app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
+	app.use(
+		express.static(path.join(import.meta.dirname, 'public'), staticOptions),
+	);
+	app.use(express.json());
+
+	// session touch
+	app.use((req, _res, next) => {
+		if (req.user) {
+			const idleTimeout = 2 * 24 * 60 * 60 * 1000; // 48 hours
+
+			const expires = req.session.cookie.expires?.getTime() ?? Date.now();
+
+			req.session.cookie.maxAge =
+				Date.now() + idleTimeout > expires ? expires - Date.now() : idleTimeout;
 		}
 		next();
-	} catch (rejected) {
-		if (rejected instanceof RateLimiterRes) {
-			res.status(429).json({
-				success: false,
-				message: 'Too many requests',
-			});
-		} else {
-			next(rejected);
-		}
-	}
-});
-
-const corsOptions = {
-	origin:
-		process.env.NODE_ENV === 'production'
-			? /helog\.whitesgr03\.com$/
-			: /localhost/,
-	methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-	credentials: true,
-	allowedHeaders: ['Content-Type', 'X-CSRF-TOKEN'],
-	exposedHeaders: ['Retry-After', 'Expire-After'],
-	maxAge: 10,
-	preflightContinue: false,
-	optionsSuccessStatus: 204,
-};
-const sessionOptions: SessionOptions = {
-	secret: process.env.SESSION_SECRETS?.split(','),
-	resave: false,
-	saveUninitialized: false, // If the user first send request to the server, at the end of the request and when saveUninitialized is false, the req.session is unmodified then will not be stored in the session store.
-	store: sessionStore.create({
-		client: mongoose.connection.getClient(),
-		stringify: false,
-	}),
-	name: process.env.NODE_ENV === 'production' ? '__Secure-id' : 'id',
-	cookie: {
-		sameSite: 'strict',
-		domain: process.env.DOMAIN ?? '',
-		httpOnly: true,
-		secure: process.env.NODE_ENV === 'production',
-		maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
-	},
-};
-const helmetOptions: HelmetOptions = {
-	xFrameOptions: { action: 'deny' }, // To avoid clickjacking attacks, by ensuring that their content is not embedded into other sites.
-	strictTransportSecurity: {
-		//  It should only be accessed using HTTPS, instead of using HTTP.
-		maxAge: 63072000,
-		preload: true,
-	},
-	crossOriginEmbedderPolicy: true, // A document can only load resources from the same origin.
-	crossOriginResourcePolicy: { policy: 'same-site' }, // Limit current resource loading to the site and sub-domains only.
-	xPoweredBy: false,
-};
-
-const staticOptions = {
-	maxAge: '1d',
-	index: false,
-	redirect: false,
-};
-
-app.set('trust proxy', 1);
-app.disable('x-powered-by');
-
-app.use(cors(corsOptions));
-app.use(helmet(helmetOptions));
-app.use(session(sessionOptions));
-app.use(passport.session());
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
-app.use(
-	express.static(path.join(import.meta.dirname, 'public'), staticOptions),
-);
-app.use(express.json());
-
-// session touch
-app.use((req, _res, next) => {
-	if (req.user) {
-		const idleTimeout = 2 * 24 * 60 * 60 * 1000; // 48 hours
-
-		const expires = req.session.cookie.expires?.getTime() ?? Date.now();
-
-		req.session.cookie.maxAge =
-			Date.now() + idleTimeout > expires ? expires - Date.now() : idleTimeout;
-	}
-	next();
-});
-
-app.get('/', (_req, res) => {
-	res.redirect(process.env.HELOG_URL);
-});
-app.use('/account', accountRouter);
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
-
-// Unknown routes handler
-app.use(((_req, res) => {
-	res.status(404).json({
-		success: false,
-		message: 'The endpoint you are looking for cannot be found.',
 	});
-}) as RequestHandler);
 
-// Errors handler
-app.use(((err, _req, res, _next) => {
-	server('has an error occur.');
-	if (err instanceof Error) server(`error message: ${err.message}`);
-	server(`error detail: ${err}`);
-
-	res.status(500).json({
-		success: false,
-		message: 'The server encountered an unexpected condition.',
+	app.get('/', (_req, res) => {
+		res.redirect(process.env.HELOG_URL);
 	});
-}) as ErrorRequestHandler);
+	app.use('/account', accountRouter);
+	app.use('/user', userRouter);
+	app.use('/blog', blogRouter);
+
+	// Unknown routes handler
+	app.use(((_req, res) => {
+		res.status(404).json({
+			success: false,
+			message: 'The endpoint you are looking for cannot be found.',
+		});
+	}) as RequestHandler);
+
+	// Errors handler
+	app.use(((err, _req, res, _next) => {
+		server('has an error occur.');
+		if (err instanceof Error) server(`error message: ${err.message}`);
+		server(`error detail: ${err}`);
+
+		res.status(500).json({
+			success: false,
+			message: 'The server encountered an unexpected condition.',
+		});
+	}) as ErrorRequestHandler);
+
+	return app;
+};
+
+export default createApp;
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,51 @@
-import mongoose from 'mongoose';
 import { server } from './utils/loggers.js';
-import './utils/checkEnv.js';
-import { app } from './app.js';
+import checkEnv from './utils/checkEnv.js';
+import connectDB from './config/database.js';
+import initialPassport from './config/passport.js';
+import createApp from './app.js';
 
 const port = process.env.PORT || 4000;
 
-mongoose.connection.on('connected', () => {
-	app
-		.listen(port, () => server(`is listened.`))
-		.on('error', error => {
-			server(`has an error occur.`);
-			if ('code' in error) {
-				switch (error.code) {
-					case 'EACCES':
-						server(`port ${port} requires elevated privileges`);
-						break;
-					case 'EADDRINUSE':
-						server(`port ${port} is already in use`);
-						break;
-					default:
-						server(`process exit with error code: ${error.code}`);
-						process.exit(1);
-				}
-			} else {
-				if (error instanceof Error) server(`error message: ${error.message}`);
-				server(`error detail: ${error}`);
+const ENV_VARS = [
+	'DB',
+	'SERVER',
+	'FACEBOOK_CLIENT_ID',
+	'FACEBOOK_CLIENT_SECRET',
+	'GOOGLE_CLIENT_ID',
+	'GOOGLE_CLIENT_SECRET',
+	'HELOG_API_URL',
+	'HELOG_URL',
+	'DATABASE_STRING',
+	'DOMAIN',
+	'CSRF_SECRETS',
+	'MAILGUN_API_KEY',
+	'MAILGUN_DOMAIN',
+];
+
+checkEnv(ENV_VARS);
+
+await connectDB();
+
+initialPassport();
+
+createApp()
+	.listen(port, () => server(`is listened.`))
+	.on('error', error => {
+		server(`has an error occur.`);
+		if ('code' in error) {
+			switch (error.code) {
+				case 'EACCES':
+					server(`port ${port} requires elevated privileges`);
+					break;
+				case 'EADDRINUSE':
+					server(`port ${port} is already in use`);
+					break;
+				default:
+					server(`process exit with error code: ${error.code}`);
+					process.exit(1);
 			}
-		});
-});
+		} else {
+			if (error instanceof Error) server(`error message: ${error.message}`);
+			server(`error detail: ${error}`);
+		}
+	});
