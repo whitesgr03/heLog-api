ae6f8e89603a5ca21475f4e95403f1641484ed47
Weiss Bai <whitesgr03@gmail.com>
2026-05-08 13:08:04 +0800

    refactor: export async function to connect DB instead of side-effects import

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 import { database } from '../utils/loggers.js';
 
-mongoose.connection
-	.on('connecting', () => database('connecting...'))
-	.on('connected', () => database('is connected.'))
-	.on('disconnected', () => database('is disconnected.'))
-	.on('error', err => database('has an error occurs: ', err));
+const connectDB = async () => {
+	mongoose.connection
+		.on('connecting', () => database('connecting...'))
+		.on('connected', () => database('is connected.'))
+		.on('disconnected', () => database('is disconnected.'))
+		.on('error', err => database('has an error occurs: ', err));
 
-mongoose
-	.connect(process.env.DATABASE_STRING)
-	.catch((err: Error) => database('has an error occur in connecting: ', err));
+	await mongoose
+		.connect(process.env.DATABASE_STRING)
+		.catch((err: Error) => database('has an error occur in connecting: ', err));
+};
+
+export default connectDB;
